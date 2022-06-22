88a3bc08e245d15b1639953538442541221aa7b2
heinzGuderian953 <80409210+heinzGuderian953@users.noreply.github.com>
2022-06-22 20:07:44 +0300

    home page

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,5 @@
 import React, {useState}  from 'react';
-import { View, Modal, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Modal, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import { globalStyles } from '../styles/global';
 import { MaterialIcons } from '@expo/vector-icons';
 import LoginScreen from './login'
@@ -38,7 +38,48 @@ export default function Home({ navigation }) {
             and allows owners to carry on the necessary activities to complete work,
             making it lean operation.
           </Text>
-          {/* <Image></Image> */}
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/img1.jpeg')}/>
+        </View>
+
+        <View style={styles.section3}>
+          <Text style={styles.primaryText}>Simple Administration</Text>
+          <Text style={styles.secondaryText}>BareProject will only ask you for less than a handful of
+           data points and fills the gap with calculated objective information that makes most impact.
+            We make a pledge that BareProject gives you more than you give it. Record maintenance and 
+            administering is always a breeze.
+          </Text>
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/img2.jpeg')}/>
+        </View>
+
+        <View style={styles.section4}>
+          <Text style={styles.primaryText}>Transparent Ownership</Text>
+          <Text style={styles.secondaryText}>Projects and deliverables are viewable by the whole team 
+          for true collaboration and enforeced sense of support. You see your peer's assigned work. 
+          If any is behind, you can nudge her to take a closer look before it is too late. And you 
+          can expect the same from others.
+          </Text>
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/img3.jpeg')}/>
+        </View>
+
+        <View style={styles.section5}>
+          <Text style={styles.primaryText}>Smart Reporting</Text>
+          <Text style={styles.secondaryText}>The most valuable insights are there and ready for 
+          consumption without prior setup and customizations. Get quick access to velocity charts, 
+          forecasted work, lead time, and activity measure across any combination of projects and owners.
+          </Text>
+          <Image 
+            style={styles.image}
+            source={require('../assets/images/img4.jpeg')}/>
+        </View>
+
+        <View style={styles.footer}>
+          <Text style={styles.center}>{'\u00A9'} 2022 Fanooce LLC</Text>
         </View>
       </ScrollView>
 
@@ -80,6 +121,40 @@ const styles = StyleSheet.create({
     backgroundColor: 'grey',
     paddingHorizontal: 20,
     paddingVertical: 100,
+    margin: 0,
+  },
+  section3:{
+    backgroundColor: 'lightgrey',
+    paddingHorizontal: 20,
+    paddingVertical: 100,
+    margin: 0,
+  },
+  section4:{
+    backgroundColor: 'grey',
+    paddingHorizontal: 20,
+    paddingVertical: 100,
+    margin: 0,
+  },
+  section5:{
+    backgroundColor: 'lightgrey',
+    paddingHorizontal: 20,
+    paddingVertical: 100,
+    margin: 0,
+  },
+  footer:{
+    backgroundColor: '#33ccff',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    margin: 0,
+  },
+  center:{
+    textAlign:'center',
+  },
+  image:{
+    width: '100%',
+    height: 300,
+    resizeMode: 'stretch',
+    padding: 0,
     margin: 0,
   },
 });
--- styles/global.js
+++ styles/global.js
@@ -12,6 +12,5 @@ export const globalStyles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    padding: 20,
   },
 });
